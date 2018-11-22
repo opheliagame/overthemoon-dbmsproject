2eb67f4a9fa14319966f19ac7bac3fa00288157a
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2018-11-22 12:30:02 +0530

    html

--- html/transaction.php
+++ html/transaction.php
@@ -1,3 +1,9 @@
+<?php
+
+include_once '../php/func.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -7,33 +13,95 @@
   <body>
     <div id="transc">
       FOOD
+      <br>
+      <input id="food1" type="radio" name="food" value=""><br>
+      <input id="food2" type="radio" name="food" value=""><br>
+      <input id="food3" type="radio" name="food" value=""><br>
 
-      <input id="food1" type="radio" name="" value="">
+
       <?php
       $sql="SELECT * FROM FOOD WHERE THEMEID=1;";
       $result=mysqli_query($con,$sql);
       $resultcheck=mysqli_num_rows($result);
       if($resultcheck > 0)
       {
-        while($row=mysqli_fetch_assoc($result))
-        echo $row['THEMEID']."<br>";
+        while($row=mysqli_fetch_assoc($result)){
+
+          echo $row['ITEM']. " $" . $row['PRICE'] ."<br>";
+        }
+        
       }
       ?>
 
-      <input id="food2" type="radio" name="" value="">
-      <input id="food3" type="radio" name="" value=""><br>
-    VENUE
-    <input id="venue1" type="radio" name="" value="">
-    <input id="venue2" type="radio" name="" value="">
-    <input id="venue3" type="radio" name="" value=""><br>
+    VENUE<br>
+    <input id="venue1" type="radio" name="venue" value=""><br>
+    <input id="venue2" type="radio" name="venue" value=""><br>
+    <input id="venue3" type="radio" name="venue" value=""><br>
+
+    <?php
+      $sql="SELECT * FROM VENUE WHERE THEMEID=1;";
+      $result=mysqli_query($con,$sql);
+      $resultcheck=mysqli_num_rows($result);
+      if($resultcheck > 0)
+      {
+        while($row=mysqli_fetch_assoc($result)){
+
+          echo $row['ITEM']. " $" . $row['PRICE'] ."<br>";
+        }
+        
+      }
+      ?>
     DECORATIONS
-    <input id="deco1" type="radio" name="" value="">
-    <input id="deco2" type="radio" name="" value="">
-    <input id="deco3" type="radio" name="" value=""><br>
+    <br>
+    <input id="deco1" type="radio" name="decorations" value=""><br>
+    <input id="deco2" type="radio" name="decorations" value=""><br>
+    <input id="deco3" type="radio" name="decorations" value=""><br>
+
+    <?php
+      $sql="SELECT * FROM DECORATIONS WHERE THEMEID=1;";
+      $result=mysqli_query($con,$sql);
+      $resultcheck=mysqli_num_rows($result);
+      if($resultcheck > 0)
+      {
+        while($row=mysqli_fetch_assoc($result)){
+
+          echo $row['ITEM']. " $" . $row['PRICE'] ."<br>";
+        }
+        
+      }
+      ?>
+
     ENTERTAINMENT
-    <input id="entn1" type="radio" name="" value="">
-    <input id="entn2" type="radio" name="" value="">
-    <input id="entn3" type="radio" name="" value=""><br>
+    <br>
+    <input id="entn1" type="radio" name="entertainment" value=""><br>
+    <input id="entn2" type="radio" name="entertainment" value=""><br>
+    <input id="entn3" type="radio" name="entertainment" value=""><br>
+
+    <?php
+      $sql="SELECT * FROM ENTERTAINMENT WHERE THEMEID=1;";
+      $result=mysqli_query($con,$sql);
+      $resultcheck=mysqli_num_rows($result);
+      if($resultcheck > 0)
+      {
+        while($row=mysqli_fetch_assoc($result)){
+
+          echo $row['ITEM']. " $" . $row['PRICE'] ."<br>";
+        }
+        
+      }
+      ?>
+
+      
+
     </div>
+
+    <script type="text/javascript">
+      function calculateTotal() {
+        
+      }
+    </script>
+
+    <button onclick="calculateTotal()">Place Order!</button>
+    
   </body>
 </html>
